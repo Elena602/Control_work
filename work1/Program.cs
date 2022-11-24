804f62a6281cab8c556e2b9aca5a8c93ffb461ad
Elena <lenalosyakova@gmail.com>
2022-11-24 21:19:40 +0300

    Ввели строки, столбцы

---
+++ work1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+Console.WriteLine("Введите количество строк (n): ");
+int n = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите количество столбцов (m): ");
+int m = int.Parse(Console.ReadLine() ?? "0");
+int[,] matrix = new int [n, m];
