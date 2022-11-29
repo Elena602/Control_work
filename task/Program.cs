5111ea67498e7a98f269bca51b9bf10df8e826dd
Elena <lenalosyakova@gmail.com>
2022-11-29 22:48:44 +0300

    Введениеи изначального и второго такой же длины

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,3 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+string[] countries = new string[7] {"Russia", "Japan", "Portugal", "USA", "Italy", "Armenia", "GB"};
+string[] array1 = new string[countries.Length];
