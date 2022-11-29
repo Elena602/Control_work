c18fabd130c5004c3b1f067daa3d285bb3431a47
Elena <lenalosyakova@gmail.com>
2022-11-29 22:51:28 +0300

    Метод сортировки 3-х и менее символьных строк

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 string[] countries = new string[7] {"Russia", "Japan", "Portugal", "USA", "Italy", "Armenia", "GB"};
 string[] array1 = new string[countries.Length];
+void SecondArray(string[] countries, string[] array1)
+{
+    int count = 0;
+    for (int i = 0; i < countries.Length; i++)
+    {
+    if(countries[i].Length <= 3)
+        {
+        array1[count] = countries[i];
+        count++;
+        }
+    }
+}
